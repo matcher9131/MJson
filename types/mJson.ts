5014c509029c67b08130f81e69c0b1c4aecfff5e
matcher9131 <matcher9131cd@gmail.com>
2023-12-20 12:12:16 +0900

    Rename PlayerGameResult to PlayerMatchResult

--- types/mJson.ts
+++ types/mJson.ts
@@ -1,4 +1,4 @@
-import type { PlayerGameResult } from "./playerGameResult.ts";
+import type { PlayerMatchResult } from "./playerMatchResult.js";
 import type { Game } from "./game.ts";
 
 /**
@@ -8,21 +8,21 @@ import type { Game } from "./game.ts";
 export type MJson = {
     /**
      * MJsonを区別するための数値を示します。
-     * 
+     *
      * 麻雀の内容には影響しません。
      */
     readonly id: number;
 
     /**
-     * 各プレイヤーの名前や結果を示す{@link PlayerGameResult}の配列です。
-     * 
+     * 各プレイヤーの名前や結果を示す{@link PlayerMatchResult}の配列です。
+     *
      * 起家から反時計回りの順で記述します。
      */
-    readonly players: readonly PlayerGameResult[];
+    readonly players: readonly PlayerMatchResult[];
 
     /**
      * それぞれの局を示す{@link Game}の配列です。
-     * 
+     *
      * 東1局0本場から順に記述します。
      */
     readonly games: readonly Game[];
